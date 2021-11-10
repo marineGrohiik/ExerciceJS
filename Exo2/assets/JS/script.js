e3c1e7170611acd7dd64120ec63ae9f0421a49fa
marineGrohiik <93980041+marineGrohiik@users.noreply.github.com>
2021-11-10 14:03:06 +0100

    correction

--- Exo2/assets/JS/script.js
+++ Exo2/assets/JS/script.js
@@ -2,4 +2,4 @@ var lastName = ('Doe')
 var firstName = ('John')
 var city = ('New-York')
 
-alert('\nNom : ' + lastName +  '\nPrénom : ' +  firstName +  '\nVille : ' + city) 
+alert('Nom : ' + lastName +  '\nPrénom : ' +  firstName +  '\nVille : ' + city) 
